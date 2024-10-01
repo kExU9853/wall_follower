03e5bfdafd1ab679e249104f3e986366f330eb01
amberxu <kexu985310@gmail.com>
2024-10-01 16:06:43 +0000

    change the easy.sh

--- easy.sh
+++ easy.sh
@@ -2,7 +2,8 @@
 #!/bin/bash
 
 gnome-terminal -- bash -c "ros2 launch turtlebot3_gazebo turtlebot3_maze.launch.py; exec bash"
-
+sleep 6
 gnome-terminal -- bash -c "ros2 launch turtlebot3_cartographer cartographer.launch.py use_sim_time:=True; exec bash"
+sleep 5
 gnome-terminal -- bash -c "ros2 launch wall_follower wall_follower.launch.py; exec bash"
 
